1b14eb008d5859108c3038f5edac1f7d8072718f
Bimmr <bimmr6696@gmail.com>
2016-05-20 17:10:42 -0400

    Updated MySQLManager to use HikariCP

---
+++ src/main/java/me/bimmr/bimmcore/mysql/Column.java
@@ -0,0 +1,51 @@
+package me.bimmr.bimmcore.mysql;
+
+
+/**
+ * Created by Randy on 05/20/16.
+ */
+public class Column {
+    private Integer               length;
+    private DataType type;
+    private String                name;
+
+    public Column(String name, DataType type, Integer length) {
+        this.name = name;
+        this.type = type;
+        this.length = length;
+    }
+
+    public Column(String name, DataType type, int length) {
+        this.name = name;
+        this.type = type;
+        this.length = Integer.valueOf(length);
+    }
+
+    /**
+     * @return the type
+     */
+    public DataType getDataType() {
+        return type;
+    }
+
+    /**
+     * @return the length
+     */
+    public Integer getLength() {
+        return length;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        if (length == null) return name + " " + type.toString();
+        return name + " " + type.toString() + "(" + length + ")";
+    }
+
+}
---
+++ src/main/java/me/bimmr/bimmcore/mysql/DataType.java
@@ -0,0 +1,16 @@
+package me.bimmr.bimmcore.mysql;
+
+/**
+ * Created by Randy on 05/20/16.
+ */
+public enum DataType {
+    INT,
+    CHAR,
+    VARCHAR,
+    TINYINT,
+    SMALLINT,
+    BOOLEAN,
+    BIGINT,
+    FLOAT,
+    DOUBLE;
+}
