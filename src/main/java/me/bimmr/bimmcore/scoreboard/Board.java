e4e1125e4e2535b5595841618b13b04a09c7737f
Bimmr <bimmr6696@gmail.com>
2016-05-24 13:27:37 -0400

    Finished up Board & BoardLine

--- src/main/java/me/bimmr/bimmcore/scoreboard/Board.java
+++ src/main/java/me/bimmr/bimmcore/scoreboard/Board.java
@@ -1,6 +1,7 @@
 package me.bimmr.bimmcore.scoreboard;
 
 import me.bimmr.bimmcore.BimmCore;
+import me.bimmr.bimmcore.events.timing.TimedEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -18,62 +19,161 @@ import java.util.List;
 public class Board {
 
     private List<BoardLine> lines;
-    private List<Player>    players;
-    private String          title;
     private Scoreboard      scoreboard;
     private Objective       objective;
     private BukkitTask      task;
+    private TimedEvent      timedEvent;
 
+    /**
+     * Create a Board
+     *
+     * @param title
+     */
     public Board(String title) {
-        this.title = title;
+        this(title, null);
+    }
+
+    /**
+     * Create a Board
+     *
+     * @param title
+     * @param timedEvent
+     */
+    public Board(String title, TimedEvent timedEvent) {
+
         this.scoreboard = Bukkit.getScoreboardManager().getNewScoreboard();
         this.objective = this.scoreboard.registerNewObjective("BC" + title, "dummy");
         this.objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         this.objective.setDisplayName(title);
+        setTimedEvent(timedEvent);
 
         this.lines = new ArrayList<>();
-        this.players = new ArrayList<>();
     }
 
+    /**
+     * Get the TimedEvent
+     *
+     * @return
+     */
+    public TimedEvent getTimedEvent() {
+        return timedEvent;
+    }
+
+    /**
+     * Set the TimedEvent
+     *
+     * @param timedEvent
+     */
+    public void setTimedEvent(TimedEvent timedEvent) {
+        if (timedEvent != null) {
+            this.timedEvent = timedEvent;
+            this.timedEvent.setAttachedObject(this);
+        }
+    }
+
+    /**
+     * Remove a line from the board
+     *
+     * @param lineNo
+     */
+    public void remove(int lineNo) {
+        lines.remove(lineNo);
+    }
+
+    /**
+     * Set the text of a line
+     *
+     * @param lineNo
+     * @param text
+     */
+    public void setText(int lineNo, String text) {
+        getBoardLine(lineNo).setText(text);
+    }
+
+    /**
+     * Add a BoardLine
+     *
+     * @param boardLine
+     */
     public void add(BoardLine boardLine) {
-        lines.add(boardLine);
         boardLine.setBoard(this);
+        lines.add(boardLine);
     }
 
+    /**
+     * Add a BoardLine
+     *
+     * @param line
+     */
+    public void add(String line) {
+        BoardLine boardLine = new BoardLine(line);
+        boardLine.setBoard(this);
+        lines.add(boardLine);
+    }
+
+    /**
+     * Get the lines
+     *
+     * @return
+     */
     public List<?> getLines() {
         return this.lines;
     }
 
-    public List<?> getPlayers() {
-        return this.players;
-    }
-
+    /**
+     * Get the boardline
+     *
+     * @param lineNo
+     * @return
+     */
     public BoardLine getBoardLine(int lineNo) {
         return lines.get(lineNo);
     }
 
+    /**
+     * Get the objective
+     *
+     * @return
+     */
     public Objective getObjective() {
         return this.objective;
     }
 
+    /**
+     * Get the scoreboard
+     *
+     * @return
+     */
     public Scoreboard getScoreboard() {
         return this.scoreboard;
     }
 
+    /**
+     * Add a player to see this scoreboard
+     *
+     * @param player
+     */
     public void addPlayer(Player player) {
         player.setScoreboard(this.scoreboard);
-        players.add(player);
     }
-    public void startTask(){
+
+    /**
+     * Start a task that will allow BoardLine to update
+     */
+    public void startTask() {
         task = new BukkitRunnable() {
             long time = 0;
 
             @Override
             public void run() {
+                if (timedEvent != null && time % timedEvent.getTicks() == 0)
+                    timedEvent.run();
+
                 for (BoardLine line : lines) {
                     if (line.getTimedEvent() != null && time % line.getTimedEvent().getTicks() == 0)
                         line.getTimedEvent().run();
                 }
+
                 if (time == Integer.MAX_VALUE)
                     time = 0;
                 time++;
@@ -81,4 +181,28 @@ public class Board {
         }.runTaskTimer(BimmCore.getInstance(), 0L, 1L);
     }
 
+    /**
+     * Stop the TimedEvent
+     */
+    public void stopTask() {
+        task.cancel();
+    }
+
+    /**
+     * Get the scoreboard's title
+     *
+     * @return
+     */
+    public String getTitle() {
+        return this.objective.getDisplayName();
+    }
+
+    /**
+     * Set the scoreboard's title
+     *
+     * @param title
+     */
+    public void setTitle(String title) {
+        this.objective.setDisplayName(title);
+    }
 }
--- src/main/java/me/bimmr/bimmcore/scoreboard/BoardLine.java
+++ src/main/java/me/bimmr/bimmcore/scoreboard/BoardLine.java
@@ -22,47 +22,124 @@ public class BoardLine {
     private Board      board;
     private TimedEvent timedEvent;
 
+    /**
+     * Create a BoardLine
+     *
+     * @param text
+     */
     public BoardLine(String text) {
         this(text, -1, null);
     }
 
+    /**
+     * Create a BoardLine
+     * <p>
+     * value = -1
+     *
+     * @param text
+     * @param value
+     */
     public BoardLine(String text, int value) {
         this(text, value, null);
     }
 
+    /**
+     * Create a BoardLine
+     * <p>
+     * value = -1
+     *
+     * @param text
+     * @param timedEvent
+     */
     public BoardLine(String text, TimedEvent timedEvent) {
         this(text, -1, timedEvent);
     }
 
+    /**
+     * Create a BoardLine
+     *
+     * @param text
+     * @param value
+     * @param timedEvent
+     */
     public BoardLine(String text, int value, TimedEvent timedEvent) {
         this.text = text;
         this.value = value;
         setTimedEvent(timedEvent);
     }
 
+    /**
+     * Set the board that this BoardLine belongs to
+     *
+     * @param board
+     */
     public void setBoard(Board board) {
         this.board = board;
 
         this.lineNo = board.getLines().size();
         team = board.getScoreboard().registerNewTeam("" + lineNo);
-        this.key = "" + ChatColor.COLOR_CHAR + lineNo + ChatColor.RESET;
+        if (lineNo > 9) {
+            this.key = "" + ChatColor.COLOR_CHAR + (lineNo - 9) + ChatColor.RESET + ChatColor.RESET;
+        } else
+            this.key = "" + ChatColor.COLOR_CHAR + lineNo + ChatColor.RESET;
 
         build();
     }
 
+    /**
+     * Get the text
+     *
+     * @return
+     */
+    public String getText() {
+        return this.text;
+    }
+
+    /**
+     * Set the text
+     *
+     * @param text
+     */
     public void setText(String text) {
         this.text = text;
         build();
     }
 
-    public void setValue(int value) {
-        this.value = value;
+    /**
+     * Get the value
+     *
+     * @return
+     */
+    public int getValue() {
+        return this.value;
     }
 
+    /**
+     * Set the value
+     *
+     * @param value
+     */
+    public void setValue(int value) {
+        this.value = value;
+
+        if (score != null)
+            this.score.setScore(value);
+    }
+
+    /**
+     * Get the TimedEvent
+     *
+     * @return
+     */
     public TimedEvent getTimedEvent() {
         return this.timedEvent;
     }
 
+    /**
+     * Set the TimedEvent
+     *
+     * @param timedEvent
+     */
     public void setTimedEvent(TimedEvent timedEvent) {
         if (timedEvent != null) {
             this.timedEvent = timedEvent;
@@ -70,6 +147,9 @@ public class BoardLine {
         }
     }
 
+    /**
+     * Build the BoardLine
+     */
     public void build() {
 
         Iterator<String> splitText;
@@ -79,16 +159,18 @@ public class BoardLine {
             splitText = Splitter.fixedLength(text.length()).split(text).iterator();
 
         team.setPrefix(splitText.next());
-        if(splitText.hasNext())
+        if (splitText.hasNext())
             team.setSuffix(splitText.next());
 
-        if(!team.getEntries().contains(key))
+        if (!team.getEntries().contains(key))
             team.addEntry(key);
-        if(score == null)
+        if (score == null)
             score = board.getObjective().getScore(key);
 
         if (value == -1)
             score.setScore(16 - lineNo);
+        else
+            score.setScore(value);
 
     }
 
