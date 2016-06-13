cfb0658db1198ddf7873f881b0b304db5cd18a39
Bimmr <bimmr6696@gmail.com>
2016-06-12 23:38:07 -0400

    Cleaned up

--- src/main/java/me/bimmr/bimmcore/commands/CommandHandler.java
+++ src/main/java/me/bimmr/bimmcore/commands/CommandHandler.java
@@ -13,93 +13,94 @@ import java.util.List;
 
 public class CommandHandler implements TabCompleter, CommandExecutor {
 
-	private ArrayList<SubCommand>	commands;
-	private JavaPlugin plugin;
-    private String command;
-	private FancyMessage information;
-	private String unknownCommand;
+    private ArrayList<SubCommand> commands;
+    private JavaPlugin            plugin;
+    private String                command;
+    private FancyMessage          information;
+    private String                unknownCommand;
 
-	public CommandHandler(JavaPlugin plugin, String command, FancyMessage information, String unknownCommand) {
-		this.commands = new ArrayList<SubCommand>();
-		this.plugin = plugin;
+    public CommandHandler(JavaPlugin plugin, String command, FancyMessage information, String unknownCommand) {
+        this.commands = new ArrayList<SubCommand>();
+        this.plugin = plugin;
         this.command = command;
-		this.information = information;
-		this.unknownCommand = unknownCommand;
+        this.information = information;
+        this.unknownCommand = unknownCommand;
 
         plugin.getCommand(command).setExecutor(this);
         plugin.getCommand(command).setTabCompleter(this);
-	}
+    }
 
-	public SubCommand getSubcommand(String command) {
-		for (SubCommand sc : commands)
-			if (sc.getName().equalsIgnoreCase(command)) return sc;
-		return null;
-	}
+    public SubCommand getSubcommand(String command) {
+        for (SubCommand sc : commands)
+            if (sc.getName().equalsIgnoreCase(command)) return sc;
+        return null;
+    }
 
-	public SubCommand getSubCommand(String cmdName) {
-		for (SubCommand cmd : this.commands)
-			if (cmd.getName().equalsIgnoreCase(cmdName)) return cmd;
-		return null;
-	}
+    public SubCommand getSubCommand(String cmdName) {
+        for (SubCommand cmd : this.commands)
+            if (cmd.getName().equalsIgnoreCase(cmdName)) return cmd;
+        return null;
+    }
 
-	public ArrayList<SubCommand> getSubCommands() {
-		return this.commands;
-	}
+    public ArrayList<SubCommand> getSubCommands() {
+        return this.commands;
+    }
 
-	public boolean isSubCommand(String command) {
-		return getSubCommand(command) != null;
-	}
+    public boolean isSubCommand(String command) {
+        return getSubCommand(command) != null;
+    }
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
-		if (cmd.getName().equalsIgnoreCase(command)) {
-			Player p = null;
-			if (sender instanceof Player) p = (Player) sender;
+        if (cmd.getName().equalsIgnoreCase(command)) {
+            Player p = null;
+            if (sender instanceof Player) p = (Player) sender;
 
-			if (args.length == 0) {
-                if(p != null)
-    				information.send(p);
+            if (args.length == 0) {
+                if (p != null)
+                    information.send(p);
                 else
                     information.sendToConsole(sender);
-			} else {
-				for (SubCommand subCommand : this.commands)
-					if (subCommand.getName().equalsIgnoreCase(args[0]) || subCommand.getAliases().contains(args[0].toLowerCase())) {
-						subCommand.execute(sender, args);
-						return true;
-					}
-				sender.sendMessage(unknownCommand);
-			}
-		}
-		return true;
-	}
+            } else {
+                for (SubCommand subCommand : this.commands)
+                    if (subCommand.getName().equalsIgnoreCase(args[0]) || subCommand.getAliases().contains(args[0].toLowerCase())) {
+                        subCommand.execute(sender, args);
+                        return true;
+                    }
+                sender.sendMessage(unknownCommand);
+            }
+        }
+        return true;
+    }
 
-	@Override
-	public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command cmd, String alias, String[] args) {
 
-		if (args.length == 1) {
-			List<String> cmds = new ArrayList<String>();
+        if (args.length == 1) {
+            List<String> cmds = new ArrayList<String>();
 
-			for (SubCommand subCommand : this.commands)
-				cmds.add(subCommand.getName());
+            for (SubCommand subCommand : this.commands)
+                cmds.add(subCommand.getName());
 
-			return TabCompletionHelper.getPossibleCompletionsForGivenArgs(args, cmds);
-		}
+            return TabCompletionHelper.getPossibleCompletionsForGivenArgs(args, cmds);
+        } else {
+            List<String> params = new ArrayList<String>();
+            for (SubCommand subCommand : this.commands)
+                if (subCommand.getName().equalsIgnoreCase(args[0]) || subCommand.getAliases().contains(args[0].toLowerCase()))
+                    params = subCommand.getTabs(args);
+            if (params != null && !params.isEmpty())
+                return TabCompletionHelper.getPossibleCompletionsForGivenArgs(args, params);
+            else
+                return TabCompletionHelper.getPossibleCompletionsForGivenArgs(args, TabCompletionHelper.getOnlinePlayers());
+        }
+    }
 
-		else {
-			List<String> params = new ArrayList<String>();
-			for (SubCommand subCommand : this.commands)
-				if (subCommand.getName().equalsIgnoreCase(args[0]) || subCommand.getAliases().contains(args[0].toLowerCase())) params = subCommand.getTabs(args);
-			if (params != null && !params.isEmpty()) return TabCompletionHelper.getPossibleCompletionsForGivenArgs(args, params);
-			else return TabCompletionHelper.getPossibleCompletionsForGivenArgs(args, TabCompletionHelper.getOnlinePlayers());
-		}
-	}
+    public void registerSubCommand(SubCommand subCommand) {
+        if (!isSubCommand(subCommand.getName())) this.commands.add(subCommand);
+    }
 
-	public void registerSubCommand(SubCommand subCommand) {
-		if (!isSubCommand(subCommand.getName())) this.commands.add(subCommand);
-	}
-
-	public void unRegisterSubCommand(SubCommand subCommand) {
-		this.commands.remove(subCommand);
-	}
+    public void unRegisterSubCommand(SubCommand subCommand) {
+        this.commands.remove(subCommand);
+    }
 }
