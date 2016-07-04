e2e9b877932d44bbb68d12df5f602b5350e6ab34
Bimmr <bimmr6696@gmail.com>
2016-07-03 23:08:22 -0400

    Added Super and SuperSub Command classes

---
+++ src/main/java/me/bimmr/bimmcore/commands/SuperCommand.java
@@ -0,0 +1,119 @@
+package me.bimmr.bimmcore.commands;
+
+import me.bimmr.bimmcore.messages.FancyMessage;
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Randy on 07/03/16.
+ */
+public abstract class SuperCommand extends SubCommand {
+
+    private List<SubCommand> subCommands;
+
+    public SuperCommand(String name) {
+        super(name);
+        subCommands = new ArrayList<>();
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args) throws CommandException {
+        if (args.length > 1) {
+            if (getSubCommand(args[1]) != null) {
+                getSubCommand(args[1]).execute(sender, args);
+            } else
+                sender.sendMessage(getInvalidSubCommandMessage());
+        } else if (sender instanceof Player)
+            getFancyMessage().send((Player) sender);
+        else
+            sender.sendMessage(getCommandExample());
+    }
+
+    public abstract String getInvalidSubCommandMessage();
+
+    @Override
+    public abstract List<String> getAliases();
+
+    public abstract String getCommandExampleHeader();
+
+    @Override
+    public String getCommandExample() {
+        String example = "";
+
+        for (int i = 0; i < subCommands.size(); i++) {
+            SubCommand subCommand = subCommands.get(i);
+            if (i != 0 && i != subCommands.size() - 1)
+                example += "\n";
+            example += subCommand.getCommandExample();
+        }
+
+        return getCommandExampleHeader() + "\n" + example;
+    }
+
+    @Override
+    public FancyMessage getFancyMessage() {
+        FancyMessage fancyMessage = new FancyMessage(getCommandExampleHeader() + "\n");
+
+        for (int i = 0; i < subCommands.size(); i++) {
+            SubCommand subCommand = subCommands.get(i);
+            if (i != 0 && i != subCommands.size() - 1)
+                fancyMessage.then("\n");
+            fancyMessage.append(subCommand.getFancyMessage());
+        }
+
+        return fancyMessage;
+    }
+
+    @Override
+    public abstract String getPermission();
+
+    @Override
+    public List<String> getTabs(String[] args) {
+
+        if (args.length == 2) {
+            List<String> tabs = new ArrayList<>();
+            for (SubCommand subCommand : subCommands)
+                tabs.add(subCommand.getName());
+
+            return tabs;
+        } else if (args.length > 2 && getSubCommand(args[1]) != null) {
+            return getSubCommand(args[1]).getTabs(args);
+        }
+        return null;
+    }
+
+    /**
+     * Get the SubCommand that matches the command
+     *
+     * @param command
+     * @return
+     */
+    public SubCommand getSubCommand(String command) {
+        for (SubCommand subCommand : subCommands)
+            if (subCommand.getName().equals(command))
+                return subCommand;
+        return null;
+    }
+
+    /**
+     * Add a subcommand
+     *
+     * @param subCommand
+     */
+    public void addSubCommand(SubCommand subCommand) {
+        this.subCommands.add(subCommand);
+    }
+
+    /**
+     * Remove a subcommand
+     *
+     * @param subCommand
+     */
+    public void removeSubCommand(SubCommand subCommand) {
+        this.subCommands.remove(subCommand);
+    }
+}
---
+++ src/main/java/me/bimmr/bimmcore/commands/SuperSubCommand.java
@@ -0,0 +1,39 @@
+package me.bimmr.bimmcore.commands;
+
+import me.bimmr.bimmcore.messages.FancyMessage;
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+/**
+ * Created by Randy on 07/03/16.
+ */
+public abstract class SuperSubCommand extends SubCommand {
+
+    public SuperSubCommand(String name) {
+        super(name);
+    }
+
+    @Override
+    public abstract void execute(CommandSender sender, String[] args) throws CommandException;
+
+    @Override
+    public List<String> getAliases() {
+        return null;
+    }
+
+    @Override
+    public abstract String getCommandExample();
+
+    @Override
+    public abstract FancyMessage getFancyMessage();
+
+    @Override
+    public String getPermission() {
+        return null;
+    }
+
+    @Override
+    public abstract List<String> getTabs(String[] args);
+}
