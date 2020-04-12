ca3e257e63b982bd7ae5f2e2ba8d523ff2f61a3a
Randy Bimm <Bimmr6696@gmail.com>
2020-04-12 03:18:49 -0400

    More Cleanup
    Cleanup that got missed

--- src/main/java/me/bimmr/bimmcore/items/attributes/AttributeType.java
+++ src/main/java/me/bimmr/bimmcore/items/attributes/AttributeType.java
@@ -1,34 +1,28 @@
-/*    */ package me.bimmr.bimmcore.items.attributes;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public enum AttributeType
-/*    */ {
-/*  7 */   MAX_HEALTH("generic.maxHealth"),
-/*  8 */   FOLLOW_RANGE("generic.followRange"),
-/*  9 */   KNOCKBACK_RESISTANCE("generic.knockbackResistance"),
-/* 10 */   MOVEMENT_SPEED("generic.movementSpeed"),
-/* 11 */   ATTACK_DAMAGE("generic.attackDamage"),
-/* 12 */   ARMOR("generic.armor"),
-/* 13 */   ARMOR_THOUGHNESS("generic.armorToughness"),
-/* 14 */   ATTACK_SPEED("generic.attackSpeed"),
-/* 15 */   LUCK("generic.luck"),
-/* 16 */   JUMP_STRENGTH("horse.jumpStrength"),
-/* 17 */   SPAWN_REINFORCEMENTS("zombie.spawnReinforcements");
-/*    */   
-/*    */   private String name;
-/*    */   
-/*    */   AttributeType(String name) {
-/* 22 */     this.name = name;
-/*    */   }
-/*    */   
-/*    */   public String getName() {
-/* 26 */     return this.name;
-/*    */   }
-/*    */ }
+package me.bimmr.bimmcore.items.attributes;
 
-
-/* Location:              C:\Users\bimmr\Downloads\BimmCore (1).jar!\me\bimmr\bimmcore\items\attributes\AttributeType.class
- * Java compiler version: 7 (51.0)
- * JD-Core Version:       1.1.3
+/**
+ * Created by Randy on 05/12/16.
  */
+public enum AttributeType {
+    MAX_HEALTH("generic.maxHealth"),
+    FOLLOW_RANGE("generic.followRange"),
+    KNOCKBACK_RESISTANCE("generic.knockbackResistance"),
+    MOVEMENT_SPEED("generic.movementSpeed"),
+    ATTACK_DAMAGE("generic.attackDamage"),
+    ARMOR("generic.armor"),
+    ARMOR_THOUGHNESS("generic.armorToughness"),
+    ATTACK_SPEED("generic.attackSpeed"),
+    LUCK("generic.luck"),
+    JUMP_STRENGTH("horse.jumpStrength"),
+    SPAWN_REINFORCEMENTS("zombie.spawnReinforcements");
+
+    private String name;
+
+    private AttributeType(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
