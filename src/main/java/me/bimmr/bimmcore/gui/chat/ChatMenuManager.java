23dc7a8151bbc000e5aac4f21329bb91be229d6d
Randy Bimm <Bimmr6696@gmail.com>
2020-04-28 14:54:47 -0400

    Removed Example Class

--- src/main/java/me/bimmr/bimmcore/gui/chat/ChatMenuManager.java
+++ src/main/java/me/bimmr/bimmcore/gui/chat/ChatMenuManager.java
@@ -4,51 +4,51 @@ import me.bimmr.bimmcore.messages.fancymessage.FancyClickEvent;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-
-class ChatMenuExample {
-    public ChatMenuExample() {
-
-        Player player = null;
-        final String[] arenaNames = new String[]{"Arena1", "Arena2", "Arena3"};
-
-        ChatMenu main = new ChatMenu().addLine("================= Edit Arenas ===============")
-                .addLine("Edit Spawns", new FancyClickEvent() {
-                            @Override
-                            public void onClick() {
-                                player.sendMessage("Editing Spawns");
-                            }
-                        }
-                ).addLine("Edit Icon", new FancyClickEvent() {
-                            @Override
-                            public void onClick() {
-                                new ChatMenu().addLine(" Edit Icon ==================")
-                                        .addLine("Show Icon", new FancyClickEvent() {
-                                            @Override
-                                            public void onClick() {
-                                                player.sendMessage("Icon");
-                                            }
-                                        })
-                                        .addLine("Set Icon", new FancyClickEvent() {
-                                            @Override
-                                            public void  onClick() {
-                                                new ChatMenu().addLine("======== Set Icon =====")
-                                                        .addLine("Something", new FancyClickEvent() {
-                                                            @Override
-                                                            public void onClick() {
-                                                                player.sendMessage("Something Else");
-                                                            }
-                                                        }).show(player);
-                                            }
-                                        })
-                                        .show(player);
-                            }
-                        }
-                )
-                .show(player);
-
-        main.destroy();
-    }
-}
+//
+//class ChatMenuExample {
+//    public ChatMenuExample() {
+//
+//        Player player = null;
+//        final String[] arenaNames = new String[]{"Arena1", "Arena2", "Arena3"};
+//
+//        ChatMenu main = new ChatMenu().addLine("================= Edit Arenas ===============")
+//                .addLine("Edit Spawns", new FancyClickEvent() {
+//                            @Override
+//                            public void onClick() {
+//                                player.sendMessage("Editing Spawns");
+//                            }
+//                        }
+//                ).addLine("Edit Icon", new FancyClickEvent() {
+//                            @Override
+//                            public void onClick() {
+//                                new ChatMenu().addLine(" Edit Icon ==================")
+//                                        .addLine("Show Icon", new FancyClickEvent() {
+//                                            @Override
+//                                            public void onClick() {
+//                                                player.sendMessage("Icon");
+//                                            }
+//                                        })
+//                                        .addLine("Set Icon", new FancyClickEvent() {
+//                                            @Override
+//                                            public void  onClick() {
+//                                                new ChatMenu().addLine("======== Set Icon =====")
+//                                                        .addLine("Something", new FancyClickEvent() {
+//                                                            @Override
+//                                                            public void onClick() {
+//                                                                player.sendMessage("Something Else");
+//                                                            }
+//                                                        }).show(player);
+//                                            }
+//                                        })
+//                                        .show(player);
+//                            }
+//                        }
+//                )
+//                .show(player);
+//
+//        main.destroy();
+//    }
+//}
 
 
 public class ChatMenuManager {
