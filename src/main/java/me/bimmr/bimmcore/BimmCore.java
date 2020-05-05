368045e1d33ce2a2e8bcfa033d91662b0eaa2768
Randy Bimm <Bimmr6696@gmail.com>
2020-05-05 10:24:01 -0400

    Fixed issue with accidentally Stashing changes

--- src/main/java/me/bimmr/bimmcore/BimmCore.java
+++ src/main/java/me/bimmr/bimmcore/BimmCore.java
@@ -1,29 +1,37 @@
 package me.bimmr.bimmcore;
 
 import me.bimmr.bimmcore.files.Config;
-import me.bimmr.bimmcore.items.Items;
-import me.bimmr.bimmcore.items.helpers.GlowEnchant;
-import me.bimmr.bimmcore.messages.fancymessage.FancyClickEvent;
-import me.bimmr.bimmcore.messages.fancymessage.FancyMessageListener;
 import me.bimmr.bimmcore.files.FileManager;
-import me.bimmr.bimmcore.timed.TimedEvent;
-import me.bimmr.bimmcore.hologram.Hologram;
-import me.bimmr.bimmcore.hologram.HologramLine;
 import me.bimmr.bimmcore.gui.book.Book;
 import me.bimmr.bimmcore.gui.chat.ChatMenu;
 import me.bimmr.bimmcore.gui.inventory.ClickEvent;
 import me.bimmr.bimmcore.gui.inventory.Menu;
 import me.bimmr.bimmcore.gui.inventory.MenuManager;
-import me.bimmr.bimmcore.messages.*;
+import me.bimmr.bimmcore.hologram.Hologram;
+import me.bimmr.bimmcore.hologram.HologramLine;
+import me.bimmr.bimmcore.items.Items;
+import me.bimmr.bimmcore.messages.ActionBar;
+import me.bimmr.bimmcore.messages.BossBar;
+import me.bimmr.bimmcore.messages.MessageDisplay;
+import me.bimmr.bimmcore.messages.Title;
+import me.bimmr.bimmcore.messages.fancymessage.FancyClickEvent;
 import me.bimmr.bimmcore.messages.fancymessage.FancyMessage;
+import me.bimmr.bimmcore.messages.fancymessage.FancyMessageListener;
+import me.bimmr.bimmcore.misc.RandomChatColor;
 import me.bimmr.bimmcore.misc.Scroller;
+import me.bimmr.bimmcore.npc.NPC;
+import me.bimmr.bimmcore.npc.NPCClickEvent;
+import me.bimmr.bimmcore.npc.NPCManager;
+import me.bimmr.bimmcore.utils.StringUtil;
 import me.bimmr.bimmcore.utils.TimeUtil;
+import me.bimmr.bimmcore.utils.timed.TimedEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.boss.BarColor;
 import org.bukkit.boss.BarStyle;
 import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -64,116 +72,6 @@ public class BimmCore extends JavaPlugin implements Listener {
         return (bcVersion >= versionNeeded);
     }
 
-    private void loadTimeUtil() {
-        FileManager fileManager = new FileManager(getInstance());
-        Config config = fileManager.getConfig("Language.yml").setup();
-        ConfigurationSection yearConfig = config.get().getConfigurationSection("Time.Year");
-        ConfigurationSection monthConfig = config.get().getConfigurationSection("Time.Month");
-        ConfigurationSection dayConfig = config.get().getConfigurationSection("Time.Day");
-        ConfigurationSection hourConfig = config.get().getConfigurationSection("Time.Hour");
-        ConfigurationSection minuteConfig = config.get().getConfigurationSection("Time.Minute");
-        ConfigurationSection secondConfig = config.get().getConfigurationSection("Time.Second");
-        TimeUtil.setIntervalStrings(new TimeUtil.Interval(yearConfig
-                .getString("Single"), yearConfig.getString("Plural"), yearConfig.getString("Short")), new TimeUtil.Interval(monthConfig
-                .getString("Single"), monthConfig.getString("Plural"), monthConfig.getString("Short")), new TimeUtil.Interval(dayConfig
-                .getString("Single"), dayConfig.getString("Plural"), dayConfig.getString("Short")), new TimeUtil.Interval(hourConfig
-                .getString("Single"), hourConfig.getString("Plural"), hourConfig.getString("Short")), new TimeUtil.Interval(minuteConfig
-                .getString("Single"), minuteConfig.getString("Plural"), minuteConfig.getString("Short")), new TimeUtil.Interval(secondConfig
-                .getString("Single"), secondConfig.getString("Plural"), secondConfig.getString("Short")));
-    }
-
-    public void onEnable() {
-        instance = this;
-        loadTimeUtil();
-
-        Bukkit.getPluginManager().registerEvents(this, this);
-        Bukkit.getPluginManager().registerEvents(new FancyMessageListener(), this);
-        Bukkit.getPluginManager().registerEvents(new MenuManager(), this);
-    }
-
-    @EventHandler
-    public void command(PlayerCommandPreprocessEvent e) {
-       /* if (e.getMessage().contains("BTest Menu")) {
-            Menu menu = new Menu("Test");
-            menu.addItem(new ItemStack(Material.GOLD_BLOCK), "Testing Add");
-            menu.setItem(1, 3, new ItemStack(Material.BOOK), "Testing Set");
-            menu.addItem(2, new ItemStack(Material.DIAMOND_BLOCK), "Testing Add", new ClickEvent() {
-                @Override
-                public void click() {
-                    e.getPlayer().sendMessage("test");
-                }
-            });
-            menu.build();
-            menu.open(e.getPlayer());
-        }
-        if (e.getMessage().contains("BTest Book")) {
-            Book book = new Book();
-            book.addLine("Test");
-            book.addBlankLine();
-            book.setLine(3, "testing Set");
-
-            book.addLine(new FancyMessage("test").onClick(new FancyClickEvent() {
-                @Override
-                public void onClick() {
-                    e.getPlayer().sendMessage("test");
-                }
-            }));
-            book.openFor(e.getPlayer());
-        }
-        if (e.getMessage().contains("BTest Chat")) {
-            ChatMenu chatMenu = new ChatMenu();
-            chatMenu.addLine("Test");
-            chatMenu.addBlankLine();
-            chatMenu.addLine("test", new FancyClickEvent() {
-                @Override
-                public void onClick() {
-                    e.getPlayer().sendMessage("test");
-                }
-            });
-            chatMenu.show(e.getPlayer());
-        }
-        if (e.getMessage().contains("BTest Hologram")) {
-            Scroller scroller = new Scroller("test message", 7, 2);
-            Hologram hologram = new Hologram(e.getPlayer().getLocation(), "Test");
-            hologram.addBlankLine();
-            hologram.addText(scroller.current(), new TimedEvent(5, this) {
-                @Override
-                public void run() {
-                    HologramLine line = (HologramLine) getAttachedObject();
-                    line.setText(scroller.next());
-                }
-            }, true);
-        }
-        if (e.getMessage().contains("BTest Action")) {
-            Scroller scroller = new Scroller("test message", 7, 2);
-            new ActionBar(scroller.current(), 10, new TimedEvent(5) {
-                @Override
-                public void run() {
-                    ((MessageDisplay) getAttachedObject()).setText(scroller.next());
-                }
-            }).send(e.getPlayer());
-        }
-        if (e.getMessage().contains("BTest Boss")) {
-            Scroller scroller = new Scroller("test message", 7, 2);
-            new BossBar(scroller.current(), 10, BarColor.GREEN, BarStyle.SEGMENTED_10, new TimedEvent(5) {
-                @Override
-                public void run() {
-                    ((MessageDisplay) getAttachedObject()).setText(scroller.next());
-                }
-            }).send(e.getPlayer());
-        }
-        if (e.getMessage().contains("BTest Title")) {
-            Scroller scroller = new Scroller("test message", 7, 2);
-            new Title(scroller.current(), 10, new TimedEvent(5) {
-                @Override
-                public void run() {
-                    ((MessageDisplay) getAttachedObject()).setText(scroller.next());
-                }
-            }).send(e.getPlayer());
-        }*/
-
-    }
-
     /**
      * Check if the server is a version
      * 1.15 -&gt; 15
@@ -206,5 +104,170 @@ public class BimmCore extends JavaPlugin implements Listener {
      */
     public static boolean supports(int v) {
         return supports(((double) v));
+    }
+
+    private void loadTimeUtil() {
+        FileManager fileManager = new FileManager(getInstance());
+        Config config = fileManager.getConfig("Language.yml").setup();
+        ConfigurationSection yearConfig = config.get().getConfigurationSection("Time.Year");
+        ConfigurationSection monthConfig = config.get().getConfigurationSection("Time.Month");
+        ConfigurationSection dayConfig = config.get().getConfigurationSection("Time.Day");
+        ConfigurationSection hourConfig = config.get().getConfigurationSection("Time.Hour");
+        ConfigurationSection minuteConfig = config.get().getConfigurationSection("Time.Minute");
+        ConfigurationSection secondConfig = config.get().getConfigurationSection("Time.Second");
+        TimeUtil.setIntervalStrings(new TimeUtil.Interval(yearConfig
+                .getString("Single"), yearConfig.getString("Plural"), yearConfig.getString("Short")), new TimeUtil.Interval(monthConfig
+                .getString("Single"), monthConfig.getString("Plural"), monthConfig.getString("Short")), new TimeUtil.Interval(dayConfig
+                .getString("Single"), dayConfig.getString("Plural"), dayConfig.getString("Short")), new TimeUtil.Interval(hourConfig
+                .getString("Single"), hourConfig.getString("Plural"), hourConfig.getString("Short")), new TimeUtil.Interval(minuteConfig
+                .getString("Single"), minuteConfig.getString("Plural"), minuteConfig.getString("Short")), new TimeUtil.Interval(secondConfig
+                .getString("Single"), secondConfig.getString("Plural"), secondConfig.getString("Short")));
+    }
+
+    public void onEnable() {
+        instance = this;
+        loadTimeUtil();
+        try {
+            NPCManager.getNPCPacketListener().start(instance);
+        } catch (Exception e) {
+            e.printStackTrace();
+            Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_RED + "NPCPacketListener Unable to Load - Did the server get reloaded?");
+        }
+        Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getPluginManager().registerEvents(new FancyMessageListener(), this);
+        Bukkit.getPluginManager().registerEvents(new MenuManager(), this);
+    }
+
+    public void onDisable() {
+        NPCManager.getNPCPacketListener().stop();
+    }
+
+    @EventHandler
+    public void command(PlayerCommandPreprocessEvent e) {
+        if (true && e.getMessage().startsWith("/BTest")) {
+            if (e.getMessage().contains("Menu")) {
+                Menu menu = new Menu("Test");
+                menu.addItem(new ItemStack(Material.GOLD_BLOCK), "Testing Add");
+                menu.setItem(1, 3, new ItemStack(Material.BOOK), "Testing Set");
+                menu.addItem(2, new ItemStack(Material.DIAMOND_BLOCK), "Testing Add", new ClickEvent() {
+                    @Override
+                    public void click() {
+                        e.getPlayer().sendMessage("test");
+                    }
+                });
+                menu.build();
+                menu.open(e.getPlayer());
+            }
+            if (e.getMessage().contains("Book")) {
+                Book book = new Book();
+                book.addLine("Test");
+                book.addBlankLine();
+                book.setLine(3, "testing Set");
+
+                book.addLine(new FancyMessage("test").onClick(new FancyClickEvent() {
+                    @Override
+                    public void onClick() {
+                        e.getPlayer().sendMessage("test");
+                    }
+                }));
+                book.show(e.getPlayer());
+            }
+            if (e.getMessage().contains("Chat")) {
+                System.out.println(FancyMessageListener.chats.size());
+                ChatMenu previous = null;
+                new ChatMenu("[ McInfected Kit "+ChatColor.GREEN+"Private "+ChatColor.WHITE+"]", new FancyMessage(ChatColor.DARK_RED+" << Back ").onClick(new FancyClickEvent() {
+                    @Override
+                    public void onClick() {
+                        if(previous != null)
+                            previous.show(getPlayer());
+                    }
+                }), ChatColor.DARK_GREEN)
+                        .addBlankLine()
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✎" + ChatColor.DARK_GRAY + "]").tooltip("Click to set to item in hand").then("  ").then(ChatColor.AQUA + "Icon: ").then(ChatColor.GRAY+"Diamond_Sword").showItem(new Items(new ItemStack(Material.DIAMOND_SWORD)).addEnchantment(Enchantment.DAMAGE_ALL, 1).getItem()))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "⧉" + ChatColor.DARK_GRAY + "]").tooltip("Click to modify setting").onClick(new FancyClickEvent() {
+                            @Override
+                            public void onClick() {
+
+                            }
+                        }).then("  ").then(ChatColor.GREEN + "Description"))
+                        .addBlankLine()
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✎" + ChatColor.DARK_GRAY + "]").tooltip("Click to set to item in hand").then("  ").then(ChatColor.GREEN + "Helmet: ").then(ChatColor.GRAY+"Diamond_Helmet").showItem(new Items(new ItemStack(Material.DIAMOND_HELMET)).getItem()))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✎" + ChatColor.DARK_GRAY + "]").tooltip("Click to set to item in hand").then("  ").then(ChatColor.GREEN + "Chestplate: ").then(ChatColor.GRAY+"Diamond_Chestplate").showItem(new Items(new ItemStack(Material.DIAMOND_CHESTPLATE)).addEnchantment(Enchantment.DAMAGE_ALL, 1).getItem()))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✎" + ChatColor.DARK_GRAY + "]").tooltip("Click to set to item in hand").then("  ").then(ChatColor.GREEN + "Leggings: ").then(ChatColor.GRAY+"Diamond_Leggings").showItem(new Items(new ItemStack(Material.DIAMOND_LEGGINGS)).addEnchantment(Enchantment.DAMAGE_ALL, 1).getItem()))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✎" + ChatColor.DARK_GRAY + "]").tooltip("Click to set to item in hand").then("  ").then(ChatColor.GREEN + "Boots: ").then(ChatColor.GRAY+"Diamond_Boots").showItem(new Items(new ItemStack(Material.DIAMOND_BOOTS)).addEnchantment(Enchantment.DAMAGE_ALL, 1).getItem()))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "⧉" + ChatColor.DARK_GRAY + "]").tooltip("Click to set to item in hand").then("  ").then(ChatColor.GREEN + "Inventory"))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "⧉" + ChatColor.DARK_GRAY + "]").then("  ").then(ChatColor.YELLOW + "Effects"))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "⧉" + ChatColor.DARK_GRAY + "]").then("  ").then(ChatColor.BLUE + "KillStreaks"))
+
+                        .addBlankLine()
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✎" + ChatColor.DARK_GRAY + "]").then("  ").then(ChatColor.LIGHT_PURPLE + "Rename Kit "))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✕" + ChatColor.DARK_GRAY + "]").then("  ").then(ChatColor.RED + "Unload Kit"))
+                        .addLine(new FancyMessage(ChatColor.DARK_GRAY + "[" + ChatColor.GREEN + "✖" + ChatColor.DARK_GRAY + "]").then("  ").then(ChatColor.DARK_RED + "Delete Kit"))
+
+                        .show(e.getPlayer());
+            }
+            if (e.getMessage().contains("Hologram")) {
+                Scroller scroller = new Scroller("test message", 7, 2);
+                Hologram hologram = new Hologram(e.getPlayer().getLocation(), "Test");
+                hologram.addBlankLine();
+                hologram.addText(scroller.current(), new TimedEvent(5, this) {
+                    @Override
+                    public void run() {
+                        HologramLine line = (HologramLine) getAttachedObject();
+                        line.setText(scroller.next());
+                    }
+                }, true);
+            }
+            if (e.getMessage().contains("Action")) {
+                Scroller scroller = new Scroller("test message", 7, 2);
+                new ActionBar(scroller.current(), 10, new TimedEvent(5) {
+                    @Override
+                    public void run() {
+                        ((MessageDisplay) getAttachedObject()).setText(scroller.next());
+                    }
+                }).send(e.getPlayer());
+            }
+            if (e.getMessage().contains("Boss")) {
+                Scroller scroller = new Scroller("test message", 7, 2);
+                new BossBar(scroller.current(), 10, BarColor.GREEN, BarStyle.SEGMENTED_10, new TimedEvent(5) {
+                    @Override
+                    public void run() {
+                        ((MessageDisplay) getAttachedObject()).setText(scroller.next());
+                    }
+                }).send(e.getPlayer());
+            }
+            if (e.getMessage().contains("Title")) {
+                Scroller scroller = new Scroller("test message", 7, 2);
+                new Title(scroller.current(), 10, new TimedEvent(5) {
+                    @Override
+                    public void run() {
+                        ((MessageDisplay) getAttachedObject()).setText(scroller.next());
+                    }
+                }).send(e.getPlayer());
+            }
+            if (e.getMessage().contains("NPC")) {
+                NPC npc = new NPC("Bimmr", e.getPlayer().getLocation());
+                npc.setNPCClickEvent(new NPCClickEvent() {
+                    @Override
+                    public void onRightClick() {
+                        if (getPlayer().isSneaking()) {
+                            getNPC().setSkin("brenden23");
+                            getNPC().refresh();
+                        } else {
+                            getNPC().setSkin("Notch");
+                            getNPC().refresh();
+                        }
+                    }
+
+                    @Override
+                    public void onLeftClick() {
+                        getNPC().setSkin("eyJ0aW1lc3RhbXAiOjE1ODgwOTU3NjI0MTcsInByb2ZpbGVJZCI6IjkxZjA0ZmU5MGYzNjQzYjU4ZjIwZTMzNzVmODZkMzllIiwicHJvZmlsZU5hbWUiOiJTdG9ybVN0b3JteSIsInNpZ25hdHVyZVJlcXVpcmVkIjp0cnVlLCJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYTlkNTAwMGQ1OGQ3NzczYTViOTYwNjZlMDVlNTdjMmY3ZDc1MWM3YWNkYWFjOTdhYWFlZmIxODEwYzQ5MzMxZiJ9fX0=",
+                                "kxjU/PtVywX6Xs36RLIwH9j18ROeANCULxy3gLTid3YSgqGzGOKYDkWaSCL+8QtYFSNd6DTtfvDxswf9DF3+NXEe0ud9qjaYT+r9QHPvl3GvdTHiqavlYmo/QduiR7r0azck3MnbAbRDhUIGxmnMhltOHraY51HEydkWLtNzFiwJt5CcWBN1X5Q8H52fzG2yhPr+rgQ3+zPedDNT6bA8b5++JKsqS8Lf+e1qkBRWSncQByIfEz+X0/1xO4mcm/Xik/88suzfyWq87RM0fbu7PKzgeXTb6JfMReA2v5SjsNGWAUqLQmtqgW8rf63/QHmiacDLl8X0Rzrtu+gpM3dRqubXzvTRz87dxfhVybFPd5OOoiK6jGQL2NGmcLhcZMzJYV3I/ELWfcfsqcLL3hKEBg2Tu6cVimFTVhx7n0lbQvD+de5lNN4xe1lpD+4v2n+gdTauKQyqZxoTpOqIZ8bXaokZUP9UldzLhFYuiV+KTgaBGgLqWaFvckz7GRUKexUkU07y/12QoCVp9XHmC1G8wzW+LS8i1P3ka4wOArn5HE2L9SZWhN4ah31eQDh9ITtpFjqgJj4y5ydZdBanMVnqHlXRz2jsgDFmgysg5njdzpyhRbkMVe+jzxnJSfngKxQkKgi/Q2ReEWCk3TXwGBseA1Y/0z2CTMo8W3UP64sWOgk=");
+                        getNPC().refresh();
+                    }
+                });
+            }
+            e.setCancelled(true);
+        }
     }
 }
